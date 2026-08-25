62f3b14a7891a91f09ae8d34d6f56644dc276e9a
neptune <ratolomeudias@gmail.com>
2026-08-25 07:51:42 -0300

    feat: responsividade e alinhamento

--- script/script.js
+++ script/script.js
@@ -1,5 +1,6 @@
 window.addEventListener("load", () => {
     const intro = document.getElementById("intro-overlay");
+    if (!intro) return;
 
     setTimeout(() => {
         intro.classList.add("show-wordmark");
@@ -9,3 +10,11 @@ window.addEventListener("load", () => {
         intro.classList.add("hidden");
     }, 4400);
 });
+
+const botao_login = document.getElementById("form-login-button")
+
+if (botao_login) {
+    botao_login.addEventListener("click", function() {
+        document.location.href = "front.html"
+    })
+}
